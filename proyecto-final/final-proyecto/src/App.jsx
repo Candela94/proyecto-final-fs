14a011adc08855b553fe501e69513fc6efddbcfb
Candela94 <candelafsg@alumnos.cei.es>
2025-02-08 09:38:15 +0100

    ok

---
+++ proyecto-final/final-proyecto/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './css/App.css'
+
+import { Button } from './components/Buttons.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <Button />
+       
+    </>
+  )
+}
+
+export default App
---
+++ proyecto-final/final-proyecto/src/components/Buttons.jsx
@@ -0,0 +1,13 @@
+
+import Buttons from' ./css/Buttons.css'
+
+
+export const Button = () => {
+
+    return(
+
+        <button className='Btn-primary'>Soy un boton</button>
+        
+    )
+
+} 
